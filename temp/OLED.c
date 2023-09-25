89d2be3d7acc19405c902b55b85a670f12caa882
jannemli <143612462+jannemli@users.noreply.github.com>
2023-09-25 16:47:33 +0200

    Update OLED.c

--- temp/OLED.c
+++ temp/OLED.c
@@ -65,6 +65,9 @@ void OLED_write_command(uint8_t data){
 
 void OLED_reset_cursor(){
 	
+	OLED_set_cursor(0x00, 127, 0x00, 0x07);
+	
+	/*
 	OLED_write_command(0x21);	// Set collumn address
 	OLED_write_command(0x00);	// Reset collumn start address
 	OLED_write_command(127);	// Reset collum end address 
@@ -72,7 +75,18 @@ void OLED_reset_cursor(){
 	OLED_write_command(0x22);	// Set page address
 	OLED_write_command(0x00);	// Reset page start address 
 	OLED_write_command(0x07);	// Reset page end address
+	*/
 
+}
+
+void OLED_set_cursor(uint8_t collumn_start, uint8_t collumn_end, uint8_t page_start, uint8_t page_end){
+	OLED_write_command(0x21);	// Set collum address
+	OLED_write_command(collumn_start);	// Set collum start address
+	OLED_write_command(collumn_end);	// Set collum end address 
+
+	OLED_write_command(0x22);	// Set page address
+	OLED_write_command(page_start);	// Set page start address 
+	OLED_write_command(page_end);	// Set page end address
 }
 
 void OLED_flush(){
@@ -90,14 +104,17 @@ void OLED_set_horizontal_mode(){
 	OLED_write_command(0x00);	// horizontal
 }
 
-void OLED_write_string(char data){
+void OLED_write_string(char* data, uint32_t length){
 	//OLED_reset_cursor();
 	unsigned char byte;
-	for (int i = 0; i < 5; i++){
-		byte = pgm_read_byte(&(font5[data-32][i]));
-		printf("Collumn: %c\r\n", byte);
-		OLED_write_data(byte);
+	for (int j = 0; j < length; j ++){
+		for (int i = 0; i < 5; i++){
+			byte = pgm_read_byte(&(font5[data[j]-32][i]));
+			OLED_write_data(byte);
+		}
 	}
+	
+	
 	/*
 	// Exclamation mark
 	for(int j=0;j<1;j++){
@@ -109,3 +126,33 @@ void OLED_write_string(char data){
 		//printf("%d", font5[data-32]);
 	}*/
 }
+
+
+void OLED_menu(){
+	OLED_flush();
+	OLED_reset_cursor();
+	OLED_set_cursor(0x04, 127, 0x00, 0x07);
+	OLED_write_string((char*)"Menu", 4);
+	
+	OLED_set_cursor(0x04, 127, 0x02, 0x07);
+	OLED_write_string((char*)"Start game", 10);
+	
+	OLED_set_cursor(0x04, 127, 0x03, 0x07);
+	OLED_write_string((char*)"Settings", 8);
+}
+
+/*
+
+void OLED_printf(char* string, uint32_t length){
+	fdevopen(OLED_Transmit, OLED_Receive);
+}
+
+void OLED_Transmit(unsigned char data){
+	OLED_write_string(&data, 1);
+}
+
+unsigned char OLED_Receive( void){
+	return 0;
+}
+
+*/
