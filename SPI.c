f6088d38c3edbf82e33b63b804f52b3f12b3e073
jannemli <143612462+jannemli@users.noreply.github.com>
2023-10-02 14:13:30 +0200

    Created files for SPI and CAN communication

---
+++ SPI.c
@@ -0,0 +1,10 @@
+void SPI_init(){
+	
+	//DDRB = (1 << DDB7);
+	// Set SS low
+	// write data to SPI register (clock generator enabled)
+	// After shift one byte: SPI clock generator stops, SPIF=1
+	// If SPIE (interrupt) set in SPCR register
+	
+	
+}
